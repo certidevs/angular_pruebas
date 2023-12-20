67919639bae9ea6746f87b24ad9bcc50b902df12
Marian Gómez Blanco <marianattack@gmail.com>
2023-12-20 12:08:53 +0100

    empieza java ok

--- JAVA/HolaMundo.java
+++
@@ -1,8 +0,0 @@
-package JAVA;
-
-public class HolaMundo {
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-    }
-    
-}
---
+++ java/tipos de datos/HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+     
+    // Método principal, se llama main, es el punto de entrada de la aplicación
+    public static void main(String[] args){
+        // con sout + tabulador se escribe System.out.println();
+        System.out.println("Hola Mundo");
+    }
+ }
